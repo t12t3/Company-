735897af3ad97fc36a9505b7bae1f165e161b15c
t12t3 <95764954+t12t3@users.noreply.github.com>
2021-12-08 18:46:47 +0300

    website

---
+++ index.php
@@ -0,0 +1,83 @@
+<?php
+session_start();
+?>
+
+<html>
+
+  <head>
+    <title> Home | Golden Vision </title>
+  </head>
+
+  <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
+
+  <link rel="stylesheet" type = "text/css" href ="css/index.css">
+
+  <body>
+
+    <button onclick="topFunction()" id="myBtn" title="Go to top">
+      <span class="glyphicon glyphicon-chevron-up"></span>
+    </button>
+    <script type="text/javascript">
+      window.onscroll = function()
+      {
+        scrollFunction()
+      };
+
+      function scrollFunction(){
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          document.getElementById("myBtn").style.display = "block";
+        } else {
+          document.getElementById("myBtn").style.display = "none";
+        }
+      }
+
+      function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+    </script>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Golden Vision</a>
+        </div>
+
+        <div class="collapse navbar-collapse " id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li class="active" ><a href="index.php">Home</a></li>
+            <li><a href="aboutus.php">About</a></li>
+            <li><a href="contactus.php">Contact Us</a></li>
+
+          </ul>
+
+
+       </div>
+
+      </div>
+    </nav>
+
+    <div class="wide">
+      	<div class="col-xs-5 line"> 
+ <hr></div>
+        <div class="col-xs-2 logo"><img src="images/logo.jpg"> <hr> <video style="text-align:center; width: 800px;" src="images/video.mp4" autoplay controls>  </div>
+        <div class="col-xs-5 line">     
+ <hr></div>
+    </div>
+
+
+    <br>
+    <div class="orderblock">
+    <h2>Need Service?</h2>
+    <center><a class="btn btn-success btn-lg" href="customerlogin.php" role="button" > Order Now </a></center>
+    </div>
+    
+  
+</body>
+</html>
